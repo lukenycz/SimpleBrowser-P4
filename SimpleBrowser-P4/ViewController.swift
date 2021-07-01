cf2917162f8ac8f479b27a5ed2dc946174b27846
Łukasz Nycz <lnycz96@gmail.com>
2021-07-01 10:56:41 +0200

    .

--- SimpleBrowser-P4/ViewController.swift
+++ SimpleBrowser-P4/ViewController.swift
@@ -11,10 +11,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("test")
-        print("test")
-        print("test")
-        print("test")
     }
 
 
